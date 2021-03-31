d19f214f501b56954145915d056ef9e7f3979b0b
veronicaguerrini <44610993+veronicaguerrini@users.noreply.github.com>
2021-03-31 11:11:43 +0200

    Update modifyDA.cpp

--- src/modifyDA.cpp
+++ src/modifyDA.cpp
@@ -1,14 +1,30 @@
 #include "Tools.h"
 
-// argomenti: nome_file.fna num_read(#1_F)
+/****************************
+Paired-end reads:
+
+R1
+--->
+_____________________________
+
+                      <------
+			   R2
+
+****************************/
 
 int main(int argc, char **argv) {
+	
+	if( argc != 3)
+	{
+		std::cerr << "Error usage " << argv[0] << " fileFasta nReads"  << std::endl;
+		exit(1);
+	}
 	
 	string fileFasta=argv[1]; 
 	dataTypeNSeq n; // numero di Read (#1_F)
 	sscanf(argv[2], "%u", &n);
 
-	
+	//Original DA from FASTA file with paired-end reads (R1 and R2) and their RC (R1_RC and R2_RC) in order	
 	string fileNameDA=fileFasta+".da";
 	FILE * fileDA=fopen(fileNameDA.c_str(), "rb");
 	if (fileDA==NULL){
@@ -16,6 +32,8 @@ int main(int argc, char **argv) {
 			printf("fopen failed, errno = %d\n", errno);
 			exit (EXIT_FAILURE);
 	}
+	
+	//New DA
 	string fileNameDA_mod=fileFasta+".da_mod";
 	FILE * fileDA_mod=fopen(fileNameDA_mod.c_str(), "wb");
 	if (fileDA_mod==NULL){
@@ -25,9 +43,13 @@ int main(int argc, char **argv) {
 	}
 	
 	
-	
+	//Read original DA
 	dataTypeNSeq* DAbuffer=(dataTypeNSeq*)malloc(n*sizeof(dataTypeNSeq));
 	int numcharDA=fread(DAbuffer, sizeof(dataTypeNSeq), n, fileDA);
+	
+	//map DA[i]:
+	//2n<=DA[i]<3n (R1_RC) ==> DA[i]=DA[i]-n
+	//3n<=DA[i]<4n (R2_RC) ==> DA[i]=DA[i]-3n
 	
 	while (numcharDA>0) {
 		for (dataTypeNChar i=0; i<numcharDA; i++) {
@@ -49,8 +71,10 @@ int main(int argc, char **argv) {
 	fclose(fileDA);
 	fclose(fileDA_mod);
 	
+	//Remove original DA
 	if(remove(fileNameDA.c_str()) != 0 )
 		cerr << "Error deleting file" << endl;
+	//Rename new DA
 	if (rename(fileNameDA_mod.c_str(), fileNameDA.c_str()) != 0)
 		cerr << "Error renaming file" << endl;
 	
